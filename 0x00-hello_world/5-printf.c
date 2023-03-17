cd9075350aa7647514db9d6973fa7794eb2f2d92
Nelorr <gloriaumk@email.com>
2023-03-16 20:21:39 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,1 @@
-#include <stdio.h>
-/**
- * main - This program prints a line
- * Return: 0 (Success)
- */
 
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}	
