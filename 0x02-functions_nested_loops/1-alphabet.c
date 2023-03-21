03f85a6f096f095e248bafb18dbdab24d2856622
Nelorr <gloriaumk@email.com>
2023-03-21 13:15:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Description:'prints alphabets in lowercase'
  * Return: always 0
  */
-void print_alphabet(void));
+void print_alphabet(void);
 {
 	int i;
 
@@ -13,6 +13,4 @@ void print_alphabet(void));
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
-
