a82927860cb8a43b05897f6a81fdde0d4094782e
Nelorr <gloriaumk@email.com>
2023-03-23 14:00:05 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - Print numbers 0 - 9 followed by a new line
- * Return: void
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0', c <= '9', c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
